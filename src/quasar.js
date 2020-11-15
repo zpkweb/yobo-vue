6b22051786168d578c2107e10bd878ec6d851a68
chenhaixin <axyewq@163.com>
2020-11-15 17:44:50 +0800

    First commit

---
+++ src/quasar.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+import './styles/quasar.scss'
+import 'quasar/dist/quasar.ie.polyfills'
+import '@quasar/extras/material-icons/material-icons.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {
+  }
+ })
